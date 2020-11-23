9bc6198c03c240114438d428ca273a2f03159cc6
Saad Shahid <saad.techgeek@gmail.com>
2020-11-23 22:05:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Helloworld from '@bit/sshahid.helloworld.helloworld';
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld />
+    </div>
+  );
+}
+
+export default App;
